703f5e9ca914a4af76fd5e390a957c00a242eabd
stefanledin <stefanledin@gmail.com>
2014-06-26 08:36:20 +0200

    refactor

--- lockontop.jquery.js
+++ lockontop.jquery.js
@@ -1,21 +1,68 @@
 ;(function (window, document, $, undefined) {
-	$.fn.lockOnTop = function () {
-		return this.each(function () {
 
-			var $el = $(this),
-				originalPosition = $(this).offset().top,
-				$document = $(document),
-				fixed = false,
-				cachedFixedValue;
+	var 
+		defaults = {
+			distance: 0
+		};
 
-			$(window).on('scroll', function () {
-				cachedFixedValue = fixed;
-				fixed = ($document.scrollTop() >= originalPosition) ? true : false;
-				if (cachedFixedValue !== fixed) {
-					$el.toggleClass('fixed');
+	function lockOnTop (element, options) {
+		this.options = $.extend( {}, defaults, options);
+
+		this.el = element;
+		this.$el = $(element);
+		this.$document = $(document);
+		this.$body = $('body');
+
+		this.init();
+	}
+
+	lockOnTop.prototype.init = function() {
+		this.scrollObserver();
+	};
+
+	lockOnTop.prototype.insertStandIn = function() {
+		var height = _this.$el.height();
+		if (this.options.) {};
+		_this.$standIn = $('<div/>').css('height', _this.$el.height());
+		_this.$standIn.insertAfter(_this.$el);
+	};
+
+	lockOnTop.prototype.removeStandIn = function() {
+		_this.$standIn.remove();
+	};
+
+	lockOnTop.prototype.scrollObserver = function() {
+		var
+			_this = this,
+			fixed = false,
+			originalPosition = this.$el.offset().top,
+			previousFixedValue = false;
+
+		$(window).on('scroll', function () {
+			fixed = (_this.$document.scrollTop() >= originalPosition) ? true : false;
+			if (fixed !== previousFixedValue) {
+				if (fixed) {
+					_this.$el.css({
+						'position': 'fixed',
+						'top': _this.options.distance
+					}).addClass('js-locked-on-top');
+					_this.insertStandIn();
+				} else {
+					_this.$el.css('position', 'static').removeClass('js-locked-on-top');
+					_this.removeStandIn();
 				}
-			});
-			
+				previousFixedValue = fixed;
+			}
 		});
 	};
+
+	$.fn.lockOnTop = function (options) {
+		return this.each(function () {
+            if (!$.data(this, 'plugin_' + lockOnTop)) {
+                $.data(this, 'plugin_' + lockOnTop, 
+                new lockOnTop( this, options ));
+            }
+        });
+	};
+
 })(window, document, jQuery);
